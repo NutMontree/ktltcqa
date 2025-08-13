ef4ab1d3b4b59fdf00e7ffe98912082e12fd5d3d
NutMontree <nutmontree29@gmail.com>
2025-08-13 15:47:55 +0700

    'v1'

---
+++ src/app/(components)/TicketCard.jsx
@@ -0,0 +1,67 @@
+import StatusDisplay from "./StatusDisplay";
+import PriorityDisplay from "./PriorityDisplay";
+import DeleteBlock from "./DeleteBlock";
+import ProgressDisplay from "./ProgressDisplay";
+import Link from "next/link";
+
+const TicketCard = ({ ticket }) => {
+  function formatTimestamp(timestamp) {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleString("en-US", options);
+
+    return formattedDate;
+  }
+
+  const createdDateTime = formatTimestamp(ticket.createdAt);
+
+  return (
+    <>
+      <div className="flex flex-col p-2 m-2 border-b">
+        <DeleteBlock id={ticket._id} />
+        <div className="font-bold text-xl">{ticket.title}</div>
+        <div className="whitespace-pre-wrap">{ticket.description}</div>
+        <div className="flex flex-col">
+          <p className="text-xs  my-1">{createdDateTime}</p>
+        </div>
+      </div>
+      {/* <div className="flex flex-col hover:bg-card-hover bg-card rounded-2xl shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay priority={ticket.priority} />
+        <div className="ml-auto">
+          <DeleteBlock id={ticket._id} />
+        </div>
+      </div>
+      <Link href={`/TicketPage/${ticket._id}`} style={{ display: "contents" }}>
+        <div className="mb-1 flex">
+          <img src="avatar.webp" alt="avatar" className="p-2 w-14" />
+          <div className="pt-4">{ticket.title}</div>
+        </div>
+        <hr className="h-px  border-0 bg-page mb-2 "></hr>
+        <p className="whitespace-pre-wrap">{ticket.description}</p>
+
+        <div className="flex-grow"></div>
+        <div className="flex mt-2">
+          <div className="flex flex-col">
+            <p className="text-xs  my-1">{createdDateTime}</p>
+            <ProgressDisplay progress={ticket.progress} />
+          </div>
+          <div className="ml-auto  flex items-end">
+            <StatusDisplay status={ticket.status} />
+          </div>
+        </div>
+      </Link>
+    </div> */}
+    </>
+  );
+};
+
+export default TicketCard;
---
+++ src/app/page.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import TicketCard from "./(components)/TicketCard";
+import DeleteBlock from "./(components)/DeleteBlock";
+
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
+
+export default async function Page() {
+  const data = await getTickets();
+
+  // Make sure we have tickets needed for production build.
+  if (!data?.tickets) {
+    return <p>No tickets.</p>;
+  }
+
+  const tickets = data.tickets;
+
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
+
+  return (
+    <div className="p-5">
+      <div>
+        <div className="text-xl font-bold">
+          Q & A และช่องทางรับฟังความคิดเห็นวิทยาลัยเทคนิคกันทรลักษ์
+        </div>
+        <div className="">วิทยาลัยเทคนิคกันทรลักษ์</div>
+        {tickets &&
+          uniqueCategories.map((uniqueCategory, categoryIndex) => (
+            <div
+              key={categoryIndex}
+              className="flex flex-col hover:bg-card-hover bg-card rounded-2xl shadow-lg p-3 m-2"
+            >
+              {/* <hr /> */}
+              <div className="grid grid-cols-2">
+                <div className="flex justify-start">
+                  <img src="avatar.webp" alt="avatar" className="p-2 w-14" />
+                  <div className="pt-4">{uniqueCategory}</div>
+                </div>
+                <div className="flex justify-end pt-5">
+                  <DeleteBlock />
+                </div>
+              </div>
+              <div className=" ">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, index) => (
+                    <TicketCard
+                      id={filteredTicket.id}
+                      key={filteredTicket.id}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
+
+        {/* {tickets &&
+          uniqueCategories.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{uniqueCategory}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4 ">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, index) => (
+                    <TicketCard
+                      id={filteredTicket.id}
+                      key={filteredTicket.id}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))} */}
+      </div>
+    </div>
+  );
+}
